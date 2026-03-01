e36cf2e395fb6ccdc0172df570be9b00bcc8975e
Madhu Vyshnavi <madhuvyshnavi1234@gmail.com>
2026-03-01 19:41:09 +0530

    Updated JavaScript code.

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 function validateform(event){
+    event.preventDefault();
+
     let isValid = true;
+
     const uname = document.getElementById("un");
     const uemail = document.getElementById("ue");
     const upassword = document.getElementById("up");
     const ugender = document.getElementById("ug");
+
+    document.getElementById("nv").textContent = "";
+    document.getElementById("ev").textContent = "";
+    document.getElementById("pv").textContent = "";
+    document.getElementById("gv").textContent = "";
+    document.getElementById("success").textContent = "";
+
     if (uname.value.trim() === ""){
         document.getElementById("nv").textContent = "Name required";
         document.getElementById("nv").style.color = "red";
         isValid = false;
     }
+
     if (uemail.value.trim() === ""){
         document.getElementById("ev").textContent = "Email required";
         document.getElementById("ev").style.color = "red";
         isValid = false;
     }
-    if (upassword.value.trim().length < 6){
+
+    if (upassword.value.trim() === ""){
+        document.getElementById("pv").textContent = "Password required";
+        document.getElementById("pv").style.color = "red";
+        isValid = false;
+    }
+    else if (upassword.value.length < 6){
         document.getElementById("pv").textContent = "Minimum 6 characters required";
         document.getElementById("pv").style.color = "red";
         isValid = false;
     }
-    if (ugender.value.trim() === ""){
+
+    if (ugender.value === ""){
         document.getElementById("gv").textContent = "Select gender";
         document.getElementById("gv").style.color = "red";
         isValid = false;
     }
+
     if (isValid){
-        window.alert("Registration Successful!");
+        alert("Registration Successful!");
         document.getElementById("success").textContent = "Registration Successful!";
         document.getElementById("success").style.color = "green";
     }
-    if (!isValid) {
-        event.preventDefault();
-    }
-    return isValid;
 }
